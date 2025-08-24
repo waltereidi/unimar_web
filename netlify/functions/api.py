26e193a96a5075c0f7b2fd2c7eec343b162d4ca8
walter <walter-eidi@hotmail.com>
2025-08-24 18:05:49 -0300

    1c

--- netlify/functions/api.py
+++ netlify/functions/api.py
@@ -1,6 +1,5 @@
 from flask import Flask, jsonify
-from werkzeug.test import EnvironBuilder
-from werkzeug.wrappers import Request
+from serverless_wsgi import handle_request
 
 app = Flask(__name__)
 
@@ -9,21 +8,4 @@ def hello():
     return jsonify(message="Hello from Flask on Netlify!")
 
 def handler(event, context):
-    """Adaptador para Netlify Function"""
-    path = event.get("path", "/")
-    method = event.get("httpMethod", "GET")
-    headers = event.get("headers") or {}
-    body = event.get("body") or ""
-
-    builder = EnvironBuilder(path=path, method=method, headers=headers, data=body)
-    env = builder.get_environ()
-    req = Request(env)
-
-    # Executa o Flask para essa request
-    resp = app.full_dispatch_request()
-
-    return {
-        "statusCode": resp.status_code,
-        "headers": dict(resp.headers),
-        "body": resp.get_data(as_text=True),
-    }
+    return handle_request(app, event, context)
